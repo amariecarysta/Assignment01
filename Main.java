8375c4716fbcc2f8fc40c918396ddc5dc4c99dfb
amariecarysta <drollina@oregonstate.edu>
2024-10-09 17:05:18 -0700

    initial commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+        public static void main(String[] args) {
+            System.out.println("Oh haaaaiiii!!!");
+            System.out.println("Hi again!");
+        }
+    }
+}
