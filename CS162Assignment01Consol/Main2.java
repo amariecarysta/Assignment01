63550688c259e51c4a1252ea4c0e475aeeb27f11
amariecarysta <drollina@oregonstate.edu>
2024-10-09 20:42:35 -0700

    Prompt user

--- CS162Assignment01Consol/Main2.java
+++ CS162Assignment01Consol/Main2.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 public class Main2 {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your favorite fruit? ");
+        String name = scanner.nextLine();
+
         if(args.length > 0) {
             if (realNum(args[0])) {
 
                 int inputAmount = Integer.parseInt(args[0]);
+
+
+                //System.out.println(name + inputAmount);
+
+
 
 
             for (int i = 0; i < inputAmount; i++ ) {
